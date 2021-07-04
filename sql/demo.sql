c0fc0016c330f0be7523abf046380a955b7464a3
ocean_wll <ocean_wll@163.com>
2021-07-04 14:57:52 +0800

    上传sql

---
+++ sql/demo.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `back_account`
+(
+    `id`            bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
+    `gmt_create`    datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`  datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `rmb_amount`    decimal(10, 0) NOT NULL DEFAULT '0' COMMENT '人民币账号金额',
+    `dollar_amount` decimal(10, 0) NOT NULL DEFAULT '0' COMMENT '美元金额',
+    `user_id`       bigint unsigned NOT NULL COMMENT '用户id',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='账号表';
+CREATE TABLE `freeze`
+(
+    `id`                   bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `gmt_create`           datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`         datetime       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `rmb_freeze_amount`    decimal(10, 0) NOT NULL DEFAULT '0' COMMENT '人民币冻结金额',
+    `dollar_freeze_amount` decimal(10, 0) NOT NULL DEFAULT '0' COMMENT '美元冻结金额',
+    `user_id`              bigint unsigned NOT NULL COMMENT '用户id',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='资金冻结表';
+
+insert into back_account(rmb_amount,dollar_amount,user_id) values (700,100,1);
+insert into back_account(rmb_amount,dollar_amount,user_id) values (700,100,2);
